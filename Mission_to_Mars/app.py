b2f5366e19c40cefc958b8a55bd063ff3c3bf935
Jon Wolgamott <Jon.Wolgamott@gmail.com>
2020-06-18 14:04:46 -0700

    setup some new files

---
+++ Mission_to_Mars/app.py
@@ -0,0 +1,29 @@
+#Import libraries
+from flask import Flask, render_template
+from flask_pymongo import flask_pyMongo
+import scrape_mars
+
+#Flask Setup
+app = Flask(__name__)
+
+#Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
+mongo = PyMongo(app)
+
+#Create the home page
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+#Create a site that collects all the scrapped stuff
+@app.route("/scrape")
+def scraper():
+    mars = mongo.db.listings
+    mars_data = scrape_mars.scrape()
+    mars.update({}, mars_data, upsert=True)
+    return redirect("/", code=302)
+
+#Needed to make everything work
+if __name__ == "__main__":
+    app.run(debug=True)
